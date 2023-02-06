1a22946f17b2d04ae41a5c37d49f4237e2864f1f
ulizellbeck <uli@modellagenten.de>
2023-02-06 21:00:38 +0100

    test

--- 02-elasticsearch-seed-main/elasticsearch-seed-script.sh
+++ 02-elasticsearch-seed-main/elasticsearch-seed-script.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 echo ELASTICSEARCH_URL: "$ELASTICSEARCH_URL"
-echo INDEX_NAME: "$INDEX_NAME"
-echo USER: "$USER"
-echo PASSWORD: "$PASSWORD"
 
-until $(curl -XGET --insecure --user $USER:$PASSWORD "$ELASTICSEARCH_URL/_cluster/health?wait_for_status=green" > /dev/null); do
+until $(curl -XGET "$ELASTICSEARCH_URL/_cluster/health?wait_for_status=green" > /dev/null); do
     printf 'WAITING FOR THE ELASTICSEARCH ENDPOINT BE AVAILABLE, trying again in 5 seconds \n'
     sleep 5
 done
@@ -17,5 +14,5 @@ do
      filename=$(basename $f)
      template_id="${filename%.*}"
      echo "Loading $template_id template..."
-     curl -s  -H 'Content-Type: application/json' -XPUT --insecure --user $USER:$PASSWORD http://168.119.189.95:9201/_template/$template_id -d@$f
+     curl -s  -H 'Content-Type: application/json' -XPUT "$ELASTICSEARCH_URL:9201/_template/$template_id" -d@$f
 done
